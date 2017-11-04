c86ab55cf0b50c1653564b9341c04e5e55849d25
Metyril <martin.clement@dbmail.com>
2017-11-04 14:58:33 +0100

    Update Symbole.h

--- Symbole.h
+++ Symbole.h
@@ -12,10 +12,10 @@ public:
 	Symbole(const string & s = "");  // Construit le symbole à partir de la chaine (string) ch lue par le lecteur de symbole dans un fichier
 	bool operator == (const string & ch) const ; // Pour tester l'égalité entre le symbole et une chaîne
 	inline bool operator != (const string  & ch) const { return ! (*this == ch); } // Pour tester la différence...
-	inline const string & getChaine() const {return m_chaine;} // Accesseur
+	inline const string & getChaine() const { return m_chaine;} // Accesseur
 	friend ostream & operator <<(ostream & cout, const Symbole & symbole); // Fonction amie pour pouvoir afficher un symbole sur cout 
 
-private:
+protected:
     	enum m_categorie { MOTCLE, VARIABLE, ENTIER, CHAINE, INDEFINI, FINDEFICHIER };
 	string           m_chaine;             // Chaîne du symbole
 	m_categorie      m_categorie;          // Categorie du symbole (voir type énuméré ci-dessus)
