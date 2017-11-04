e22b160ceac978d780a01f69634fe801a4c8a64a
Metyril <martin.clement@dbmail.com>
2017-11-04 19:25:28 +0100

    Update ArbreAbstrait.h

--- ArbreAbstrait.h
+++ ArbreAbstrait.h
@@ -210,4 +210,24 @@ class NoeudChaine : public Noeud {
   private:
     Noeud* m_chaine;
 };
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+class NoeudInstSelon : public Noeud {
+// Classe pour représenter un noeud "instruction siRiche"
+//  et ses 2 fils : la condition du si et la séquence d'instruction associée
+  public:
+    NoeudInstSelon(Noeud* variable, vector<Noeud*> expressions, vector<Noeud*> sequences);
+     // Construit une "instruction si" avec sa condition et sa séquence d'instruction
+    ~NoeudInstSelon() {} // A cause du destructeur virtuel de la classe Noeud
+    void traduitEnCPP(ostream & cout, unsigned int indentation) const; //traduit la fonction selon en C++
+    int executer();  // Exécute l'instruction si : si condition vraie on exécute la séquence
+    //void traduitEnCPP(ostream & cout, unsigned int indentation) const; //traduit la fonction ecrire en C++
+
+  private:
+    Noeud* m_variable;
+    vector<Noeud*>  m_expressions;
+    vector<Noeud*>  m_sequences;
+};
 #endif /* ARBREABSTRAIT_H */
