777c5e21d9569999a8a754ccb902b57b21f08de5
Metyril <martin.clement@dbmail.com>
2017-11-05 10:43:40 +0100

    Update Interpreteur.h

--- Interpreteur.h
+++ Interpreteur.h
@@ -9,63 +9,53 @@
 
 class Interpreteur {
 public:
-	Interpreteur(ifstream & fichier);   // Construit un interpréteur pour interpreter
-	                                    //  le programme dans  fichier 
-                                      
-	void analyse();                     // Si le contenu du fichier est conforme à la grammaire,
-	                                    //   cette méthode se termine normalement et affiche un message "Syntaxe correcte".
-                                      //   la table des symboles (ts) et l'arbre abstrait (arbre) auront été construits
-	                                    // Sinon, une exception sera levée
+    Interpreteur(ifstream & fichier);   // Construit un interpréteur pour interpreter
+                                        // et le programme dans fichier.
 
-	inline const TableSymboles & getTable () const  { return m_table;    } // accesseur	
-	inline Noeud* getArbre () const { return m_arbre; }                    // accesseur
-        void traduitEnCPP(ostream & cout,unsigned int indentation) const;
+    void analyse();                     // Si le contenu du fichier est conforme à la grammaire,
+                                        // cette méthode se termine normalement et affiche un message "Syntaxe correcte".
+                                        // La table des symboles (ts) et l'arbre abstrait (arbre) auront été construits,
+                                        // sinon, une exception sera levée.
 
-	
+    inline const TableSymboles & getTable () const  { return m_table;    }  // accesseur
+    inline Noeud* getArbre () const { return m_arbre; }                     // accesseur
+
+    void traduitEnCPP(ostream & cout,unsigned int indentation) const;       // Compilation en C++
+
+
 private:
     Lecteur        m_lecteur;  // Le lecteur de symboles utilisé pour analyser le fichier
     TableSymboles  m_table;    // La table des symboles valués
     Noeud*         m_arbre;    // L'arbre abstrait
 
     // Implémentation de la grammaire
-    Noeud*  programme();   //   <programme> ::= procedure principale() <seqInst> finproc FIN_FICHIER
-    Noeud*  seqInst();	   //     <seqInst> ::= <inst> { <inst> }
-    Noeud*  inst();	       //        <inst> ::= <affectation> ; | <instSi>
-    Noeud*  affectation(); // <affectation> ::= <variable> = <expression> 
-    Noeud*  expression();  //  <expression> ::= <facteur> { <opBinaire> <facteur> }
-    Noeud*  facteur();     //     <facteur> ::= <entier>  |  <variable>  |  - <facteur>  | non <facteur> | ( <expression> )
+    Noeud*  programme();    // <programme> ::= procedure principale() <seqInst> finproc FIN_FICHIER
+    Noeud*  seqInst();      // <seqInst> ::= <inst> { <inst> }
+    Noeud*  inst();         // <inst> ::= <affectation> ; | <instSiRiche> | <instTantQue> | <instRepeter> ; | <instPour> | <instEcrire> ; | <instLire> ; | <instSelon>
+    Noeud*  affectation();  // <affectation> ::= <variable> = <expression>
     
     
-    Noeud*  terme();
-    Noeud*  expBool();
-    Noeud*  relationEt();
-    Noeud*  relation();
-                           //   <opRel> ::=   == | != | < | <= | > | >=
-                           //   <opBinaire> ::= + | - | *  | / | < | > | <= | >= | == | != | et | ou
-    Noeud*  instSi();      //      <instSi> ::= si ( <expression> ) <seqInst> finsi
-    
-    
-    
-    
-    
-    
-    
-    // RAJOUTER
-    Noeud* instTantQue();   // <instTantQue> ::= tantque ( <expression> ) <seqInst> fintantque
-    Noeud* instRepeter();   // <instRepeter> ::= repeter <seqInst> jusqua( <expression> )
-    Noeud* instPour();      // <instPour>   ::=pour( [ <affectation> ] ; <expression> ;[ <affectation> ]) <seqInst> finpour
-    Noeud* instSiRiche();   // <instSiRiche> ::= si ( <expression> ) <seqInst> {sinonsi ( <expression> ) <seqInst>} [sinon ( <expression> ) <seqInst>] finsi
+    Noeud*  expression();   // <expression> ::= <terme> { + <terme> | - <terme> }
+    Noeud*  terme();        // <terme> ::= <facteur> { * <facteur> | / <facteur> }
+    Noeud*  facteur();      // <facteur> ::= <entier> | <variable> | - <expBool> | non <expBool> | ( <expBool> )
+    Noeud*  expBool();      // <expBool> ::= <relationET> { ou <relationEt> }
+    Noeud*  relationEt();   // <relationEt> ::= <relation> { et <relation> }
+    Noeud*  relation();     // <relation> ::= <expression> { <opRel> <expression> }
+    // <opRel> ::== = | != | < | <= | > | >=
+
+
+    Noeud* instSiRiche();   // <instSiRiche> ::= si ( <expBool> ) <seqInst> { sinonsi ( <expBool> ) <seqInst> } [ sinon ( <expBool> ) <seqInst> ] finsi
+    Noeud* instTantQue();   // <instTantQue> ::= tantque ( <expBool> ) <seqInst> fintantque
+    Noeud* instRepeter();   // <instRepeter> ::= repeter <seqInst> jusqua( <expBool> )
+    Noeud* instPour();      // <instPour> ::= pour( [ <affectation> ] ; <expBool> ;[ <affectation> ]) <seqInst> finpour
     Noeud* instLire();      // <instLire> ::= lire( <variable> {, <variable> })
-    Noeud* instEcrire();    // <instEcrire> ::=ecrire( <expression> | <chaine> {, <expression> | <chaine> })    
-    
-    Noeud* instSelon();
-        
-    
-    
-    
-    
-    
-    // outils pour simplifier l'analyse syntaxique
+    Noeud* instEcrire();    // <instEcrire> ::= ecrire( <expBool> | <chaine> {, <expBool> | <chaine> })    
+
+
+    Noeud* instSelon();     // <instSelon> ::= selon ( <expBool> ) { cas <expBool> : <seqInst> } [ defaut : <seqInst> ] finselon
+
+
+    // Outils pour simplifier l'analyse syntaxique
     void tester (const string & symboleAttendu) const throw (SyntaxeException);   // Si symbole courant != symboleAttendu, on lève une exception
     void testerEtAvancer(const string & symboleAttendu) throw (SyntaxeException); // Si symbole courant != symboleAttendu, on lève une exception, sinon on avance
     void erreur (const string & mess) const throw (SyntaxeException);             // Lève une exception "contenant" le message mess
